cf1f89cf7f8def29af8fd10c20a89ae0e0bffec9
jyoti <js406587@gmail.com>
2018-06-29 15:29:50 +0530

    assignment16

--- quiz1/quiz1.py.py
+++ quiz1/quiz1.py.py
@@ -1,28 +1,21 @@
 #Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 #between 2000 and 3200 (both included).
 #The numbers obtained should be printed in a comma-separated sequence on a single line.
-#l=[]
-#for x in range(2000,3200):
-#	if  (x%7==0)and(x%5!=0):
-#		l.append(str(x))
-#print(','.join(l))	
+l=[]
+for x in range(2000,3200):
+	if  (x%7==0)and(x%5!=0):
+		l.append(str(x))
+print(','.join(l))	
 
 #Write a program which accepts a sequence of comma-separated 
 #numbers from console and generate a list and a tuple 
 #which contains every number.
-#l1=[34,67,55,33,12,98]
-#t1=(34,67,55,33,12,98)
-#l1=list(t1)
-#t1=(tuple(l1))
-#print(t1)
-#print(l1)
-
-
-
-#write a program that accepts a sentence and 
-#calculate the number of upper case and lower case letters.
+import random 
+for i in range(6):
+	n=random.randint(0,6)
+	l.sort(str(i))
 n=int(input("enter any number"))
-
+print(','.join(1))
 
 	
 
